cb82cf21bdcacd5aa46745e0b3a5597a0f4583cb
aliou1710 <diallo.aliou97@gmail.com>
2023-02-02 02:14:59 +0100

    template interpolation input keyup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,31 @@ export class AppComponent implements OnInit{
 
  ngOnInit(){
     console.table(this.pokemonlist); 
-    this.selectPokemon(this.pokemonlist[0]);
+    //this.selectPokemon(this.pokemonlist[0]);
   }
 
-  selectPokemon(pokemonObject : pokemon){
-    console.log(`Ona cliqué sur le pokemon ${pokemonObject.name}`);
+  /*selectPokemon(events : MouseEvent){
+    const index : number = Number((events.target as HTMLInputElement).value);
+    console.log(`Ona cliqué sur le pokemon ${this.pokemonlist[index].name}`);
+  }*/
+  //le ? : si le pokemonselected n'est pas defini, il ne vas pas generer d'erreur
+  Pokemonselected?:pokemon;
+
+  selectPokemon(pokemonId : string){
+    const index = Number(pokemonId);
+    //on veut chercher un pokemon qui a cet id dans la liste, s'il n'existe pas il renvoie undefined
+    const pokemonunit: pokemon|undefined = this.pokemonlist.find(pokemon_=>pokemon_.id==index);
+    this.Pokemonselected = this.pokemonlist[index];
+    if(pokemonunit){
+      //pokemon
+      console.log(`seconde methode : On a cliqué sur le pokemon ${pokemonunit.name}`);
+      this.Pokemonselected = pokemonunit;
+    }else{
+      //undefined
+      console.log(`vous avez demander un pokemon qui n'existe pas`);
+      this.Pokemonselected = pokemonunit;
+    }
+  
   }
  
 }
