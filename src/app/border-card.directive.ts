dba7d0825f8b93c173a65a0ce62fd0b3f40d3124
aliou1710 <diallo.aliou97@gmail.com>
2023-02-02 18:29:19 +0100

    personnaliser directives

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,20 +1,25 @@
-import { Directive, ElementRef ,HostListener} from '@angular/core';
+import { Directive, ElementRef ,HostListener,Input} from '@angular/core';
 
 @Directive({
   selector: '[pkmonappBorderCard]'
 })
 export class BorderCardDirective {
+  private initialColor : string ="#f5f5f5";
+  private defaulColor : string ='009688';
+  private defaultHeight: number = 180;
+
 
   constructor(private el : ElementRef) {
     //on initialise dabord nos carte
     
-    this.setBorder('#f5f5f5');
-    this.setHeight(190);
+    this.setBorder(this.initialColor);
+    this.setHeight(this.defaultHeight);
    }
-
+   @Input('pkmonappBorderCard') borderColor : string;
    //lorsque le curseur est sur la carte
    @HostListener('mouseenter') onMouseEnter(){
-    this.setBorder('#D0237A');
+    // this.setBorder('#D0237A');
+    this.setBorder(this.borderColor || this.defaulColor);
    }
    //lorsque le curseur n'est plus sur la carte, on reinitialise
    @HostListener('mouseleave') onMouseLeave(){
