891501453c2e6ce5c24f866697f126d9f2f93ee1
aliou1710 <diallo.aliou97@gmail.com>
2023-02-02 20:24:45 +0100

    routes bases

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //les routes les plus spécifiques sont en haut et les plus globales en bas
+  {path:'pokemons',component	: ListPokemonComponent},
+  {path:'pokemon/:id',component: DetailPokemonComponent },
+  //chemin par defaut
+  {path:'',redirectTo: 'pokemons',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,64 @@
+import { Component ,OnInit} from '@angular/core';
+import { pokemon } from '../pokemon';
+import {ListPOKEMONS} from '../mock-pokemon-lists';
+@Component({
+  selector: 'app-list-pokemon',
+  templateUrl: './list-pokemon.component.html',
+  styles: [
+  ]
+})
+export class ListPokemonComponent {
+  //title = 'ng-pokemon-app';
+  title = 'List de pokemons';
+  //tableau de pokemon
+  pokemonlist : pokemon[] = ListPOKEMONS
+ //pokemonlists=['pikachu','barbaroza','noah'];
+
+ ngOnInit(){
+    console.table(this.pokemonlist); 
+    //this.selectPokemon(this.pokemonlist[0]);
+  }
+
+  /*selectPokemon(events : MouseEvent){
+    const index : number = Number((events.target as HTMLInputElement).value);
+    console.log(`Ona cliqué sur le pokemon ${this.pokemonlist[index].name}`);
+  }*/
+  //le ? : si le pokemonselected n'est pas defini, il ne vas pas generer d'erreur
+  Pokemonselected?:pokemon;
+
+  selectPokemon(pokemonId : string){
+    const index = Number(pokemonId);
+    //on veut chercher un pokemon qui a cet id dans la liste, s'il n'existe pas il renvoie undefined
+    const pokemonunit: pokemon|undefined = this.pokemonlist.find(pokemon_=>pokemon_.id==index);
+    this.Pokemonselected = this.pokemonlist[index];
+    if(pokemonunit){
+      //pokemon
+      console.log(`seconde methode : On a cliqué sur le pokemon ${pokemonunit.name}`);
+      this.Pokemonselected = pokemonunit;
+    }else{
+      //undefined
+      console.log(`vous avez demander un pokemon qui n'existe pas`);
+      this.Pokemonselected = pokemonunit;
+      
+    }
+  
+  }
+
+  selectPokemonDirective(pokemon__ : pokemon){
+    const index = Number(pokemon__.id);
+    //on veut chercher un pokemon qui a cet id dans la liste, s'il n'existe pas il renvoie undefined
+    const pokemonunit: pokemon|undefined = this.pokemonlist.find(pokemon_=>pokemon_.id==index);
+    this.Pokemonselected = this.pokemonlist[index];
+    if(pokemonunit){
+      //pokemon
+      console.log(`seconde methode : On a cliqué sur le pokemon ${pokemonunit.name}`);
+      this.Pokemonselected = pokemonunit;
+    }else{
+      //undefined
+      console.log(`vous avez demander un pokemon qui n'existe pas`);
+      this.Pokemonselected = pokemonunit;
+      
+    }
+  
+  }
+}
